09da1dcf1e63e011d37dde1626bd4e94d2345144
kamlesh Thavani <kamlesh.simbanic2022@gmail.com>
2024-04-17 19:25:38 +0530

    profile api added

--- src/app/api/users/current/route.ts
+++ src/app/api/users/current/route.ts
@@ -1,10 +1,24 @@
 import { usersRepo } from "@/app/_helpers/server";
 import { apiHandler } from "@/app/_helpers/server/api";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
-module.exports = apiHandler({
-  GET: getCurrent,
-});
+// module.exports = apiHandler({
+//   GET: getCurrent,
+// });
 
 async function getCurrent() {
   return await usersRepo.getCurrent();
+}
+
+export async function GET(request: NextRequest) {
+  const reqHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  console.log(reqHeaders.get("Authorization"));
+  console.log(headerList.get("Authorization"));
+
+  return new Response("<h1>Profle Api data</h1>", {
+    headers: { "Content-Type": "text/html" },
+  });
 }
