2064b2671c6e7f0cc4c18d0d964bcd5ae09a511c
kamlesh Thavani <kamlesh.simbanic2022@gmail.com>
2024-04-17 17:18:54 +0530

    named export added

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -3,10 +3,10 @@ import joi from "joi";
 import { productRepo } from "@/app/_helpers/server";
 import { apiHandler } from "@/app/_helpers/server/api";
 
-module.exports = apiHandler({
-  GET: getAll,
-  POST: create,
-});
+// module.exports = apiHandler({
+//   GET: getAll,
+//   POST: create,
+// });
 
 async function getAll(req: Request) {
   return await productRepo.getAll();
@@ -15,4 +15,15 @@ async function getAll(req: Request) {
 async function create(req: Request) {
   const body = await req.json();
   await productRepo.create(body);
+}
+
+export async function GET() {
+  const data = await productRepo.getAll();
+  return Response.json(data);
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  await productRepo.create(body);
+  return Response.json(body);
 }
