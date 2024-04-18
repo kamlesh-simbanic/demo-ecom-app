c532666d4923c4e47469f1f3fdcd463feba04424
kamlesh Thavani <kamlesh.simbanic2022@gmail.com>
2024-04-18 17:20:28 +0530

    cookies added

--- src/app/api/users/current/route.ts
+++ src/app/api/users/current/route.ts
@@ -1,6 +1,6 @@
 import { usersRepo } from "@/app/_helpers/server";
 import { apiHandler } from "@/app/_helpers/server/api";
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 // module.exports = apiHandler({
@@ -15,10 +15,16 @@ export async function GET(request: NextRequest) {
   const reqHeaders = new Headers(request.headers);
   const headerList = headers();
 
+  cookies().set("resultPerPage", "20");
+  const theme = request.cookies.get("theme");
+
   console.log(reqHeaders.get("Authorization"));
   console.log(headerList.get("Authorization"));
 
+  console.log(theme);
+  console.log(cookies().get("resultPerPage"));
+
   return new Response("<h1>Profle Api data</h1>", {
-    headers: { "Content-Type": "text/html" },
+    headers: { "Content-Type": "text/html", "Set-Cookie": "theme=dark" },
   });
 }
