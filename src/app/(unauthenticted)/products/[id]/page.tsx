7d6ad2da3f43917b49d3064fea45bc54a3266f4f
kamlesh Thavani <kamlesh.simbanic2022@gmail.com>
2024-04-19 17:36:46 +0530

    streaming added

--- src/app/(unauthenticted)/products/[id]/page.tsx
+++ src/app/(unauthenticted)/products/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getProduct } from "@/app/_actions/product";
 import Content from "@/app/_components/products/Details";
+import { Suspense } from "react";
 
 export default async function ProductDetails({
   params,
@@ -11,7 +12,24 @@ export default async function ProductDetails({
 
   return (
     <>
+      <h1>Product Details Page</h1>
       <Content product={product} readOnly={true} />
+
+      <Suspense fallback={<p>Loading Product details</p>}>
+        <Product />
+      </Suspense>
+      <Suspense fallback={<p>Loading Reviews</p>}>
+        <Review />
+      </Suspense>
     </>
   );
 }
+
+const Product = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return <div>Product</div>;
+};
+const Review = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  return <div>Review</div>;
+};
