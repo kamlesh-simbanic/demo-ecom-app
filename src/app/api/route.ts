e3f14ac58f7c427ca7b78ca868ce7c841b156ec0
kamlesh Thavani <kamlesh.simbanic2022@gmail.com>
2024-05-23 15:17:09 +0530

    unecessary files removed

--- src/app/api/route.ts
+++
@@ -1,6 +0,0 @@
-// export const dynamic = "force-dynamic"; // defaults to auto
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-  return NextResponse.json({ msg: "helloe" }, { status: 200 });
-}
