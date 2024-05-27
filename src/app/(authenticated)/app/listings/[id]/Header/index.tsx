8707fb47a0f06d7b1b9d303d17ef5dd0b8399495
kamlesh Thavani <kamlesh.simbanic2022@gmail.com>
2024-05-27 14:58:15 +0530

    tab navigation done

--- src/app/(authenticated)/app/listings/[id]/Header/index.tsx
+++ src/app/(authenticated)/app/listings/[id]/Header/index.tsx
@@ -1,7 +1,52 @@
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Container } from "react-bootstrap";
+import { Tabs, Tab } from "react-bootstrap";
+import { Tabs as TabsT, URLs, getTab } from "../utils";
+import { SyntheticEvent, useEffect, useState } from "react";
 
-const Header = async (props: any) => {
-  return <div>Header</div>;
+const Header = async ({ id }: any) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState<string>(getTab(pathname));
+
+  useEffect(() => {
+    setActiveTab(getTab(pathname));
+  }, [pathname]);
+
+  const onTabChange = (newTab: string) => {
+    console.log("newTab", newTab);
+    console.log("URLs[newTab]", URLs[newTab]);
+
+    setActiveTab(newTab);
+    router.push(`/app/listings/${id}${URLs[newTab]}`);
+  };
+
+  const handleTabChange = (newTab: string) => {
+    onTabChange(newTab);
+  };
+
+  return (
+    <div>
+      Header
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={activeTab}
+        onSelect={(k) => handleTabChange(k!)}
+        className="mb-3"
+      >
+        <Tab title="Overview" eventKey={"Overview"} />
+        <Tab title={"Access & Issues"} eventKey={"AccessIssues"} />
+        <Tab title="Access" eventKey={"Access"} />
+        <Tab title={"Offers"} eventKey={"Offers"} />
+        <Tab title="Services" eventKey={"Services"} />
+        <Tab title="Listing Details" eventKey={"ListingDetails"} />
+        <Tab title="Property Details" eventKey={"PropertyDetails"} />
+        <Tab title="Listing Engagement" eventKey={"ListingEngagement"} />
+        <Tab title="Commissions & Expenses" eventKey={"Commissions"} />
+      </Tabs>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/app/(authenticated)/app/listings/[id]/utils.ts
@@ -0,0 +1,37 @@
+export enum Tabs {
+  Overview = "",
+  AccessIssues = "AccessIssues",
+  Access = "Access",
+  Offers = "Offers",
+  Services = "Services",
+  ListingDetails = "ListingDetails",
+  PropertyDetails = "PropertyDetails",
+  ListingEngagement = "ListingEngagement",
+  Commissions = "Commissions",
+}
+
+export const URLs: { [tab: string]: string } = {
+  [Tabs.Overview]: "",
+  [Tabs.AccessIssues]: "/access-issues",
+  [Tabs.Access]: "/access",
+  [Tabs.Offers]: "/offers",
+  [Tabs.Services]: "/services",
+  [Tabs.ListingDetails]: "/listing-details",
+  [Tabs.PropertyDetails]: "/property-details",
+  [Tabs.ListingEngagement]: "/listing-engagement",
+  [Tabs.Commissions]: "/commissions-and-expenses",
+};
+
+export const getTab = (pathname: string): Tabs => {
+  if (pathname.includes(URLs[Tabs.ListingDetails])) return Tabs.ListingDetails;
+  if (pathname.includes(URLs[Tabs.Offers])) return Tabs.Offers;
+  if (pathname.includes(URLs[Tabs.PropertyDetails]))
+    return Tabs.PropertyDetails;
+  if (pathname.includes(URLs[Tabs.Services])) return Tabs.Services;
+  if (pathname.includes(URLs[Tabs.ListingEngagement]))
+    return Tabs.ListingEngagement;
+  if (pathname.includes(URLs[Tabs.Commissions])) return Tabs.Commissions;
+  if (pathname.includes(URLs[Tabs.AccessIssues])) return Tabs.AccessIssues;
+  if (pathname.includes(URLs[Tabs.Access])) return Tabs.Access;
+  return Tabs.Overview;
+};
